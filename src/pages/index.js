aea7afd05f2a1af2dbfafca33a979682a5c312cf
fatGubari <fatimajubary@gmail.com>
2023-03-14 22:52:38 +0300

    add fetch product data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,28 @@
-export default function Home() {
+import React from "react";
+
+function Products({ products }) {
+  console.log(products);
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      {products.map((product) => {
+        return (
+          <div key={product.id}>
+            <img src={product.image} alt={product.title} />
+            <h1>{product.title}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
+export default Products;
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://fakestoreapi.com/products");
+  const data = await res.json();
+  return {
+    props: {
+      products: data,
+    },
+  };
+};
