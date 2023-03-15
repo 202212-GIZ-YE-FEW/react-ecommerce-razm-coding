b166576ef06408ffefaa261074ba60beb1676282
Abdullah Binsaad <abdullah.binsaadd@gmail.com>
2023-03-16 02:05:03 +0300

    chore: add opensans font

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Open_Sans } from 'next/font/google';
+
+const OpenSans = Open_Sans({
+	subsets: ['latin'],
+	variable: '--open-sans',
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${OpenSans.variable} font-sans`}>
+			<Component {...pageProps} />;
+		</main>
+	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+	content: [
+		'./app/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./components/**/*.{js,ts,jsx,tsx}',
 
-    // Or if using `src` directory:
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+		// Or if using `src` directory:
+		'./src/**/*.{js,ts,jsx,tsx}',
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['var(--open-sans)'],
+				...defaultTheme.fontFamily.sans,
+			},
+		},
+	},
+	plugins: [],
+};
